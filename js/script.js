1a5b75a4007703698983aed8c9c83865588bade2
Artur Rymsza <artur.rymsza@gmail.com>
2018-01-13 23:17:25 +0100

    Add js folding effect

--- js/script.js
+++ js/script.js
@@ -1,10 +1,27 @@
-/*$(function() {
-	var $topMenu = $('#top');
-	var $jumbotron = $('#jumbo');
+$(function() {
+	var $open = $('.openB');
+	var $close = $('.closeB');
+	var $left = $('.left');
+	var $right = $('.right');
+	var $back = $('.back');
 
-	function welcomeScreen () {
-		$jumbotron.css({"background-image":"url(../img/szyca_szyld.jpg)","height": "90vh"});
-		$topMenu.css({"display": "inline-block", "height":"10vh"});
-	}
-	setTimeout(welcomeScreen, 3000);
-})*/
+	$open.on('click', function(){
+		$left.addClass('open');
+		setTimeout(function(){
+			$right.addClass('open');
+		}, 250);
+		setTimeout(function(){
+			$back.addClass('open');
+		}, 350);
+	});
+
+	$close.on('click', function(){
+		setTimeout(function(){
+			$left.removeClass('open');
+		}, 250);
+		$right.removeClass('open');
+		setTimeout(function(){
+			$back.removeClass('open');
+		}, 600);
+	});
+});
